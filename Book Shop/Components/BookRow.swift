7904a37cf11ba4b3c6c8730afd2dc243f3ec0702
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-13 19:24:43 +0600

    added  book row

---
+++ Book Shop/Components/BookRow.swift
@@ -0,0 +1,40 @@
+//
+//  BookRow.swift
+//  Book Shop
+//
+//  Created by Rezaul Islam on 13/7/23.
+//
+
+import SwiftUI
+
+struct BookRow: View {
+    @EnvironmentObject var cm : CartManager
+    var book : Book
+    var body: some View {
+        HStack(spacing:20){
+            Image(book.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading, spacing:  10){
+                Text(book.name)
+                Text("Tk: \(book.price)")
+            }
+            Spacer()
+            Image(systemName: "trash")
+                .foregroundColor(.red)
+                .onTapGesture {
+                    cm.removeFromCart(book: book)
+                }
+            
+        }
+    }
+}
+
+struct BookRow_Previews: PreviewProvider {
+    static var previews: some View {
+        BookRow(book: bookList[1])
+    }
+}

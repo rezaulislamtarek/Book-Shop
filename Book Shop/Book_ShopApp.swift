dac061f97c8bea682427824f470510420a0c0c2c
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-13 16:42:28 +0600

    Initial Commit

---
+++ Book Shop/Book_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_ShopApp.swift
+//  Book Shop
+//
+//  Created by Rezaul Islam on 13/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Book_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
